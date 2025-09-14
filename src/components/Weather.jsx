011380ecfdb3e4355aa1c0164683af6907602c39
rseth06a <rseth06a@gmail.com>
2025-09-14 01:40:25 -0700

    initial commit

---
+++ src/components/Weather.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Weather.css'
+import pinkSearchIcon from '../assets/pinkSearchIcon.png'
+
+const Weather = () => {
+  return (
+    <div className='weather'>
+        <div className ="search-bar">
+            <input type="text" placeholder='Search'/>
+            <img src={pinkSearchIcon} alt="" />
+        </div>
+    </div>
+  )
+}
+
+export default Weather
